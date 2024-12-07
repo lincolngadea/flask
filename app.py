d970238bdd5345e5ffb3aa9db9020a6e3491f9c8
lincolngadea <lincolngadea@gmail.com>
2024-12-07 19:22:43 -0300

    chore: implementa renderização de formulário com flask-wtf

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask, render_template
+from form import NameForm
 
 template_dir = os.path.abspath('./templates')
-app = Flask(__name__, template_folder = template_dir)
+app = Flask(__name__, template_folder=template_dir)
 
-@app.route('/user')
-@app.route('/user/<name>')
-def index(name=None):
-    print(name)
-    return render_template('index.html', name=name)
+app.config['SECRET_KEY'] = 'ja;sdlkfj;asdlkfja;sdlfkjas;lkdfja;slkdfj'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = " "
+    return render_template('index.html', name=name, form=form)  # Corrigida a indentação
