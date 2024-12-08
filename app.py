e2f7a27ed9afaaa7ba9cdaff3a8ddc7e5f4a4c98
lincolngadea <lincolngadea@gmail.com>
2024-12-08 11:59:56 -0300

    chore: refatora o projeto e prepara para usar o sqlite

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
 from flask import Flask, render_template
 from form import NameForm
+from flask_migrate import Migrate
+from models.User import db
 
-template_dir = os.path.abspath('./templates')
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'ja;sdlkfj;asdlkfja;sdlfkjas;lkdfja;slkdfj'
+# Configuração do app
+app.config.from_object('config')
+
+# Inicialização do banco de dados e migrações
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,5 +19,5 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         name = form.name.data
-        form.name.data = " "
+        form.name.data = ""  # Limpa o campo do formulário
     return render_template('index.html', name=name, form=form)  # Corrigida a indentação
